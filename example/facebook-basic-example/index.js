851755faa9a919283c2fa3ad6505d3d6dcbec38f
Pepper Yen <pepper.yen@gmail.com>
2018-05-06 16:36:58 +0800

    Add messenger bot example

---
+++ example/facebook-basic-example/index.js
@@ -0,0 +1,51 @@
+const { MessengerBot } = require('bottender');
+const { createServer } = require('bottender/express');
+const greenlock = require('greenlock-express');
+const Drama = require('../../index');
+
+if (
+  process.env.MESSENGER_ACCESS_TOKEN &&
+  process.env.MESSENGER_APP_SECRET
+) {
+  const bot = new MessengerBot({
+    accessToken: process.env.MESSENGER_ACCESS_TOKEN,
+    appSecret: process.env.MESSENGER_APP_SECRET,
+  });
+
+  const drama = new Drama({
+    bot,
+  });
+
+  drama.addScene('TEST_KEY', {
+    type: 'BOT_REPLY',
+    trigger_type: 'text',
+    predicate: 'hello',
+    property: {
+      type: 'text',
+      text: 'World',
+    },
+  });
+
+  const server = createServer(bot);
+
+  if (
+    process.env.OWNER_EMAIL &&
+    process.env.SITE_URL
+  ) {
+    greenlock.create({
+      server: 'https://acme-v01.api.letsencrypt.org/directory',
+      email: process.env.OWNER_EMAIL,
+      agreeTos: true,
+      approveDomains: [process.env.SITE_URL],
+      app: server,
+    }).listen(80, 443);
+  } else {
+    const PORT = process.env.PORT || 8080;
+    server.listen(PORT, () => {
+      console.log(`App listening on port ${PORT}`);
+      console.log('Press Ctrl+C to quit.');
+    });
+  }
+} else {
+  console.log('Please export MESSENGER_ACCESS_TOKEN and MESSENGER_APP_SECRET first.');
+}
